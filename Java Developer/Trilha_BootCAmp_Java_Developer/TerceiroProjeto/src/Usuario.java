17861d7ca6529531c7cb9328e860383524ed2098
Leonardo Gildo <168222922+L-Gildo@users.noreply.github.com>
2025-02-20 09:02:34 -0300

    Atualização de aulas

---
+++ Java Developer/Trilha_BootCAmp_Java_Developer/TerceiroProjeto/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+    }
+}
